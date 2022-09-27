b80a383bba9ac59eec2a10dda3d4baaf606900b2
Srinivas Kota <saisrinivasa.likhit@students.iiit.ac.in>
2022-09-26 22:45:03 -0700

    update: decimal scaling in pricing

--- src/mappings/markets.ts
+++ src/mappings/markets.ts
@@ -87,7 +87,7 @@ function getTokenPrice(
     3. which block: if or else or both?
   */
   // if (blockNumber > 7715908) {
-  let mantissaDecimalFactor = 18 - underlyingDecimals + 18
+  let mantissaDecimalFactor = 18
   let bdFactor = exponentToBigDecimal(mantissaDecimalFactor)
   let oracle = PriceOracle.bind(oracleAddress)
   log.info('MARKETS::getTokenPrice {}', [oracleAddress.toHex()])
@@ -142,7 +142,7 @@ function getUsdcPriceNOTE(blockNumber: i32): BigDecimal {
   // if (blockNumber > 7715908) {
   log.info('MARKETS::getUsdcPriceNOTE {}', [oracleAddress.toHex()])
   let oracle = PriceOracle.bind(oracleAddress)
-  let mantissaDecimalFactorUSDC = 18 - 6 + 18
+  let mantissaDecimalFactorUSDC = 18
   let bdFactorUSDC = exponentToBigDecimal(mantissaDecimalFactorUSDC)
 
   let underlyingPriceResult = oracle.try_getUnderlyingPrice(
